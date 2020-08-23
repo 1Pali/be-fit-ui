4eea8304b41ac9eb8712419ed914884d0a39f3f4
Pavol Cina <pavol.cina@sap.com>
2020-08-23 10:59:05 +0200

    multer and file upload uncommented

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-// var multer = require('multer');
-// var upload = multer();
+
+//multer causing some issues at startup of app but without him data not working properly
+var multer = require('multer');
+var upload = multer();
  var path = require('path');
 //
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(upload.array());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array());
 
 app.set('port', (process.env.PORT || 8000));
 app.use(express.static(path.join(__dirname, '../main/webapp')));
